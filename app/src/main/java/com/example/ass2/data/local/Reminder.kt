719b216fcca06fca045b6fb13a3eb178d98be5d9
Ziyan Chen <zche0383@student.monash.edu>
2026-05-18 18:41:50 +1000

    Revert "Merge branch 'ziyanchen' of https://git.infotech.monash.edu/FIT5046/26S1/A24-Lab03-G07"
    This reverts commit c50909e26aec6d9a5c59890cc8094e07f33f7485, reversing
    changes made to aef7d787ff9d557756f5d80c55a9965c61d3e86a.

--- app/src/main/java/com/example/ass2/data/local/Reminder.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ass2.data.local
-
-import java.sql.Time
-
-data class Reminder (
-    val id: Int,
-    val time: Time,
-    val description: String,
-)
--- app/src/main/java/com/example/ass2/data/local/Target.kt
+++ app/src/main/java/com/example/ass2/data/local/Target.kt
@@ -1,11 +1,8 @@
 package com.example.ass2.data.local
 
-import java.sql.Date
-
 data class Target(
     val id: Int,
-    val calories: Int,
-    val date: Date,
+    val title: String,
     val points: Int,
     var completed: Boolean = false
 )
